ac42f52b7aabf02b2365253dfed43b3a62621f0f
Andras Tovishati <andras.tovishati@gmail.com>
2025-02-23 12:01:49 +0000

    removed routerlink

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -4,7 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { tap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
@@ -16,8 +16,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 		MatButtonModule,
 		MatFormFieldModule,
 		MatInputModule,
-		FormsModule,
-		RouterLink
+		FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
