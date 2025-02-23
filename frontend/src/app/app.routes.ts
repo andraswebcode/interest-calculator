3a5b2b7548532caf0eb3a9aadbe0110ceaa6faa7
Andras Tovishati <andras.tovishati@gmail.com>
2025-02-23 11:20:20 +0000

    added routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		redirectTo: 'dashboard',
+		pathMatch: 'full'
+	},{
+		path: 'login',
+		component: LoginComponent
+	},{
+		path: 'dashboard',
+		canActivate: [authGuard],
+		loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
+	}
+];
