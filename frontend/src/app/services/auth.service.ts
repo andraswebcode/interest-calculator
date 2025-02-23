7231d3a115b32fdb4ee46da8391fc3a3031e193d
Andras Tovishati <andras.tovishati@gmail.com>
2025-02-23 11:37:20 +0000

    made methods

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../types/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+	private readonly CURRENT_USER_KEY = 'currentUser';
+	private readonly _user = signal<User | null>(null);
 
-  constructor() { }
+	constructor() {
+    	try {
+			const user = JSON.parse(localStorage.getItem(this.CURRENT_USER_KEY) as string);
+			this._user.set(user);
+		} catch (e) {
+			//
+		}
+	}
+
+	get currentUser() {
+		return this._user.asReadonly();
+	}
+
+	get isLoggedIn() {
+		return this._user() !== null;
+	}
+
+	public login(username: string, password: string) {}
+
+	public logout() {
+		return new Observable<boolean>((observer) => {
+			localStorage.removeItem(this.CURRENT_USER_KEY);
+			observer.next(true);
+		});
+	}
 }
