164615f1241d5aeef27cb9e93c7fa02810aeaedb
Andras Tovishati <andras.tovishati@gmail.com>
2025-02-23 07:49:06 +0000

    set up home template

---
+++ backend/resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="{{ frontend_asset('styles.css') }}">
+</head>
+<body>
+	<app-root></app-root>
+	<script src="{{ frontend_asset('polyfills.js') }}"></script>
+	<script src="{{ frontend_asset('main.js') }}"></script>
+</body>
+</html>
