4e33a48b4722eeb4d3a3e6430b048f70a1d3b964
Andras Tovishati <andras.tovishati@gmail.com>
2025-02-24 10:56:59 +0000

    added child components

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CalculatorComponent } from '../../components/calculator/calculator.component';
+import { InterestListComponent } from '../../components/interest-list/interest-list.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CalculatorComponent, InterestListComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
