c2429eb2864f22d28c276070bb1fb8da3e462098
Andras Tovishati <andras.tovishati@gmail.com>
2025-02-24 13:34:01 +0000

    created ngoninit

--- frontend/src/app/components/interest-list/interest-list.component.ts
+++ frontend/src/app/components/interest-list/interest-list.component.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InterestCalculatorService } from '../../services/interest-calculator.service';
 import { MatList } from '@angular/material/list';
 import { InterestItemComponent } from '../interest-item/interest-item.component';
 import { Interest } from '../../types/interest';
+import { tap } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-interest-list',
-  imports: [MatList, InterestItemComponent],
+  imports: [MatCardModule, MatList, InterestItemComponent],
   templateUrl: './interest-list.component.html',
   styleUrl: './interest-list.component.scss'
 })
-export class InterestListComponent {
+export class InterestListComponent implements OnInit {
   interestList:Interest[] = [];
 
   constructor(
     private readonly icService: InterestCalculatorService
   ){}
+
+  ngOnInit(): void {
+    this.icService.getInterests().pipe(
+      tap(items => this.interestList = items)
+    ).subscribe();
+  }
 }
