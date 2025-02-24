175f803e9d59a0fbfa8f357af7f905da44f215db
Andras Tovishati <andras.tovishati@gmail.com>
2025-02-24 09:05:19 +0000

    added properties

--- backend/app/Models/Interest.php
+++ backend/app/Models/Interest.php
@@ -9,4 +9,31 @@ class Interest extends Model
 {
     /** @use HasFactory<\Database\Factories\InterestFactory> */
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'start_date',
+        'end_date',
+        'amount',
+        'interest'
+    ];
+
+    /**
+	 * Get the attributes that should be cast.
+	 *
+	 * @return array<string, string>
+	 */
+	protected function casts(): array
+    {
+        return [
+            'start_date' => 'date',
+            'end_date' => 'date',
+            'amount' => 'decimal:2',
+            'interest' => 'decimal:2'
+        ];
+    }
 }
