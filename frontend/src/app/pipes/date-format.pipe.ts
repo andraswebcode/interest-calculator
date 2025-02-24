a43464fbe9d21bbf8e245c6186c1e45dbea6c896
Andras Tovishati <andras.tovishati@gmail.com>
2025-02-24 13:36:30 +0000

    created pipe

---
+++ frontend/src/app/pipes/date-format.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateFormat'
+})
+export class DateFormatPipe implements PipeTransform {
+
+  transform(value: string): string {
+    const date = new Date(value);
+    return date.toDateString();
+  }
+
+}
